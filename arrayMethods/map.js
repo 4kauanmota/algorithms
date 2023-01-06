4071b107c58694ee6f5e934ea9f33dbfa2acc325
Kauan Soares Mota <4kauanmota@gmail.com>
2023-01-05 22:11:30 -0300

    map additions and corrections

--- arrayMethods/map.js
+++ arrayMethods/map.js
@@ -1,7 +1,15 @@
-// the map method creates a copy of the array ( that is, it doesn't change the original array that called the method)
+// EN
+// the map method runs through the array (that is, it doesn't change the original array that called the method)
 // you can return the same elements from this array to create a copy
 // you can return the elements with some modification 
 // you can even use it to display the elements of the array (but for that case there's another, more appropriate method)
+
+// PT
+// o metódo map percorre o array (ou seja, não altera o array original que chamou o metódo)
+// você pode retornar os mesmos elementos desse array para criar uma cópia
+// pode retornar os elementos com alguma modificação 
+// até pode usar para apresentar os elementos do array (mas para esse caso tem outro metódo mais apropriado)
+
 
 const array = [1, 2, 3, 4];
 
@@ -16,6 +24,7 @@ array.map(element => console.log(element + 1));
 | * 4       |
 | * 5       |
 |___________|
+
 */
 
 
@@ -27,6 +36,7 @@ console.log(copyArray);
 |------------------|
 | * [2, 4, 8, 12]  |
 |__________________|
+
 */
 
 
@@ -38,4 +48,5 @@ console.log(copyArray2);
 |------------------|
 | * [3, 5, 6, 7]   |
 |__________________|
+
 */
